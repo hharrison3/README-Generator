edc79063fff7e3f89a040afb543cc2e533ace5d7
Harbour Harrison <harb3gaucho@gmail.com>
2020-09-11 10:28:57 -0700

    updated readme layout

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -3,8 +3,16 @@ const api = require("./api");
 function generateMarkdown(data) {
   console.log(api.getUser(data.github));
   return `
-# ${data.title}
+# Github: ${data.github}
+# Email: ${data.email}
 # ![GitHub license](${api.getUser(data.github)})
+# Project Title: ${data.title}
+# Project Description: ${data.description}
+# Required License: ${data.license}
+# Command to Install Dependencies: ${data.installation}
+# Command to Run Tests: ${data.test}
+# What to know about using the Repo: ${data.usage}
+# What to know about contributing to the Repo: ${data.contributing}
 `;
 }
 
